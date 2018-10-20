b26cc016c600f12573d9cf2e45e4abcbbd1f57eb
Teju Vishwakarma <teju068@gmail.com>
2018-10-20 23:44:26 +0530

    first commit

--- src/genericstack/GenStack.java
+++ src/genericstack/GenStack.java
@@ -3,40 +3,40 @@ package genericstack;
 import static genericstack.Utils.printf;
 
 import java.util.Scanner;
+
 public class GenStack {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Stack<Integer> stack = new Stack<Integer>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
 
-		do {
-			printf("*****Generic Stack********");
-			printf("Enter you choice ==>");
-			printf("1 for Push");
-			printf("2 for Pop");
-			printf("3 for peek");
-			printf("4 for Traverse");
-			int choice = scanner.nextInt();
-			switch(choice)
-			{
-			case 1:
-				printf("Enter the data which you want to insert");
-				int data =scanner.nextInt();
-				stack.push(data);
-				break;
-			case 2:
-				int poppedData = stack.pop();
-				printf("Data : "+poppedData);
-				break;
-			case 3:
-				int topData = stack.peek();
-				printf("Check Data : "+topData);
-				break;
-			case 4:
-				//TODO
-				break;
-			}
-		}while(true);
-	}
+        do {
+            printf("*****Generic Stack********");
+            printf("Enter you choice ==>");
+            printf("1 for Push");
+            printf("2 for Pop");
+            printf("3 for peek");
+            printf("4 for Traverse");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    printf("Enter the data which you want to insert");
+                    int data = scanner.nextInt();
+                    stack.push(data);
+                    break;
+                case 2:
+                    int poppedData = stack.pop();
+                    printf("Data : " + poppedData);
+                    break;
+                case 3:
+                    int topData = stack.peek();
+                    printf("Check Data : " + topData);
+                    break;
+                case 4:
+                    //TODO
+                    break;
+            }
+        } while (true);
+    }
 
 }
--- src/genericstack/Stack.java
+++ src/genericstack/Stack.java
@@ -2,43 +2,35 @@ package genericstack;
 
 public class Stack<T> {
 
-	int size;
-	T object;
-	Node<T> start = null;
+    int size;
+    Node<T> start = null;
 
-	Stack(){
+    Stack() {
 
-	}
+    }
 
-	Stack(T object, Node<T> node){
-		this.object = object;
-		this.start = node;
-	}
 
-	public void push(T data) {
-		size++;
-		Node<T> node = new Node<T>(data, null);
-		if(start == null) {
-			start = node;
-		}else {
-			node.setNext(start);
-			start = node;
-		}
-	}
+    public void push(T data) {
+        size++;
+        Node<T> node = new Node<>(data, null);
+        if (start == null) {
+            start = node;
+        } else {
+            node.setNext(start);
+            start = node;
+        }
+    }
 
-	public T pop() {
-		size--;
-		Node<T> poppedNode;
-		poppedNode = start;
-		start = start.getNext();
-		return poppedNode.getObject();
-	}
+    public T pop() {
+        size--;
+        Node<T> poppedNode;
+        poppedNode = start;
+        start = start.getNext();
+        return poppedNode.getObject();
+    }
 
-	public T peek() {
-		return start.getObject();
-	} 
+    public T peek() {
+        return start.getObject();
+    }
 
-	public Node<T> getTop(){
-		return start;
-	}
 }
